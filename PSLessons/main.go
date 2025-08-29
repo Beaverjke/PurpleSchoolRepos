d9cb9adda6184059725989689f6ef925cc8e1d00
Alexander Bobrovsky <sania.bobrovscky@yandex.ru>
2025-08-29 23:39:48 +0500

    Upd 1.2

--- PSLessons/main.go
+++ PSLessons/main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
+
+func calcIMT(uWeight float64, uHeight float64) (float64, error) {
+	if uWeight <= 0 {
+		return 0.0, errors.New("не указан вес")
+	} else if uHeight <= 0 {
+		return 0.0, errors.New("не указана высота")
+	}
+	const IMTPower = 2
+	IMT := uWeight / math.Pow(uHeight/100, 2)
+	return IMT, nil
+}
+
+func main() {
+	//defer func() {
+	//	if r := recover(); r != nil {
+	//		fmt.Println("Recovered", r)
+	//	}
+	//}()
+	fmt.Println("_-_-_ Это калькулятор IMT _-_-_")
+	for {
+		IMT, err := calcIMT(getUserInput()) // userWeight, userHeight := getUserInput()
+		if err != nil {
+			fmt.Println(err)
+			continue
+			//panic(err)
+		}
+		IMTOutput(IMT)
+
+		isRepeatCalc := checkRestart()
+		if !isRepeatCalc {
+			break
+		}
+	}
+}
 
 func getUserInput() (float64, float64) {
 	var userHeight float64
@@ -15,30 +50,41 @@ func getUserInput() (float64, float64) {
 	return userWeight, userHeight
 }
 
-func calcIMT(uWeight float64, uHeight float64) float64 {
-	const IMTPower = 2
-	IMT := uWeight / math.Pow(uHeight/100, 2)
-	return IMT
-}
-
 func IMTOutput(imt float64) {
 	result := fmt.Sprintf("Твой IMT: %.1f", imt)
 	fmt.Println(result)
-}
 
-func main() {
-	fmt.Println("_-_-_ Это калькулятор IMT _-_-_")
-	IMT := calcIMT(getUserInput()) // userWeight, userHeight := getUserInput()
-	if IMT < 16 {
+	switch {
+	case imt < 16:
+
 		fmt.Println("Ты очень худой!")
-	} else if IMT < 18.5 {
+	case imt < 18.5:
+
 		fmt.Println("Ты худой!")
-	} else if IMT < 25 {
+	case imt < 25:
+
 		fmt.Println("Ты норм!")
-	} else if IMT < 30 {
-		fmt.Println("Ты ты пухлый!")
-	} else {
+	case imt < 30:
+		fmt.Println("Ты пухлый!")
+	default:
 		fmt.Println("Ты жирный!")
 	}
-	IMTOutput(IMT)
+}
+
+func checkRestart() bool {
+	var userAnswerValue int
+	fmt.Println(`Ты хочешь запустить калькулятор снова?
+	1 - Продолжить
+	0 - Стоп`)
+	fmt.Scan(&userAnswerValue)
+	switch {
+	case userAnswerValue == 1:
+		return true
+	}
+	return false
+
+	//if userAnswerValue == 1 {
+	//	return true
+	//}
+	//return false
 }
